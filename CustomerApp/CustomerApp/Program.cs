162031b4e85cb32e03236d8d1c2589a1e53427e5
Jenny TRIEU <jtrieu7@nait.ca>
2026-04-22 14:50:51 -0600

    summary block

---
+++ CustomerApp/CustomerApp/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+///  Create a program that gives gives information and store informations into classes 
+/// Program : CustomerDetail.cs
+/// Author : Jenny Trieu
+/// Date : 04/22/2026
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
