c32eb795c3f619aa2fc37a120cd89078a7d27e24
Jenny TRIEU <jtrieu7@nait.ca>
2026-04-22 14:49:33 -0600

    Created class

---
+++ Class1.cs
@@ -0,0 +1,102 @@
+﻿/// <summary>
+///  Create a program that gives gives information and store informations into classes 
+/// Program : CustomerDetail.cs
+/// Author : Jenny Trieu
+/// Date : 04/22/2026
+/// </summary>
+using System;
+
+public class Customer
+{
+    double _totalSales = 0;
+    int _ordercount = 0;
+
+    //Declaring Varibles 
+    string firstname { get; set; `}
+    string lastname { get; set; }
+    int ordercount
+    {
+        //Declaring Value
+        get { return _ordercount}
+        set
+        {
+            //If and else
+            if (value <= 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                _ordercount = value;
+            }
+        }
+    }
+    double totalsales
+    {
+        //Declaring Value
+        get { return _totalSales}
+        set
+        {
+            //If and else 
+            if (value < 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                _totalSales = value;
+            }
+        }
+    }
+    double AverageOrder
+    {
+        get
+        {
+            return totalsales / ordercount
+
+        }
+    }
+    string customerteir
+    {
+        get
+        {
+            // IF and Else 
+            if (ordercount < 10)
+            {
+
+                return "Bronze"
+
+
+            }
+            else if (ordercount > 49)
+            {
+
+                return "Gold"
+
+
+            }
+            else
+
+            { return "Silver"}
+        }
+
+    }
+
+    string fullname
+    {
+        get
+        {
+            return $"{lastname}, {firstname}";
+        }
+    }
+
+    public Customer(string first, string last, int count, double total)
+    {
+        //Constructor
+        firsname = first;
+        lastname = last;
+        ordercount = count;
+        totalsales = total;
+
+    }
+}
